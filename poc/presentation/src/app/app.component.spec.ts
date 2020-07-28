54b7aa47600855a17b92ffcf497fd43994b2c9f4
Romain DALICHAMP <romain.dalichamp@alithya.com>
2020-07-28 00:15:20 -0400

    Infinite Loop fix to allow SonarQube Analysis and Tests coverage

--- poc/presentation/src/app/app.component.spec.ts
+++ poc/presentation/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {} from 'jasmine';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
