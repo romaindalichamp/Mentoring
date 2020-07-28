7dc743d1cc1692243dfe49b0ac2756c187a96928
Romain DALICHAMP <romain.dalichamp@alithya.com>
2020-07-27 23:41:41 -0400

    SonarQube TS Analysis for Angular 8

--- poc/presentation/src/app/app.component.ts
+++ poc/presentation/src/app/app.component.ts
@@ -7,16 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'presentation';
-  var mysql = require('mysql');
-
-  var connection = mysql.createConnection(
-    {
-      host:'localhost',
-      user: "admin",
-      database: "project",
-      password: "mypassword", // sensitive
-      multipleStatements: true
-    });
-
-  connection.connect();
 }
